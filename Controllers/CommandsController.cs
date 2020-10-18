4720b5d098daf9138a71f9c39d19b4fe21c6096d
Martin Oshalim <martinoshalim@gmail.com>
2020-10-18 21:11:49 +1100

    Add project files.

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CommandAPI.Data;
+using CommandAPI.Dtos;
+using CommandAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        //GET api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable> GetAllCommands()
+        {
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(_repository.GetAllCommands()));
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}", Name = "GetCommandById")]
+        public ActionResult <CommandReadDto> GetCommandById(int id)
+        {
+            var result = _repository.GetCommandById(id);
+            if(result != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(result));
+            }
+
+            return NotFound();
+        }
+
+
+        //Post api/commands
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+
+            if (_repository.SaveChanges())
+            {
+                var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+                return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
+            }
+
+            return Conflict();            
+        }
+
+        //PUT api/commands/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if(commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(commandUpdateDto, commandModelFromRepo);
+            _repository.UpdateCommand(commandModelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        //PATCH api/commands/{id}
+        [HttpPatch("{id}")]
+        public ActionResult PartialCommandUpdate(int id, JsonPatchDocument<CommandUpdateDto> patchDoc)
+        {
+            // Check if command exists in repo.
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            //Apply the patch to our model by using using Microsoft.AspNetCore.JsonPatch JsonPatchDocument.ApplyTo and validate changes.
+            var commandToPatch = _mapper.Map<CommandUpdateDto>(commandModelFromRepo);
+            patchDoc.ApplyTo(commandToPatch, ModelState);
+            if(!TryValidateModel(commandToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+            
+            //Update command model with changes from patch
+            _mapper.Map(commandToPatch, commandModelFromRepo);
+
+            //Update and save changes.
+            _repository.UpdateCommand(commandModelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        //DELETE api/commands/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        {
+            // Check if command exists in repo.
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _repository.DeleteCommand(_repository.GetCommandById(id));
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CommandAPI.Dtos;
+using CommandAPI.Models;
+
+namespace CommandAPI.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            //Source -> Target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            
+            //Creates mapping in both directions
+            CreateMap<CommandUpdateDto, Command>().ReverseMap();
+
+        }
+    }
+}
